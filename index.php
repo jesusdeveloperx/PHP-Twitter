207b1c33f916786c6f9fcf6adfe513e73dceddf1
jesusdeveloperx <jesuscg1996@gmail.com>
2023-11-14 21:38:49 +0100

    config-html

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
 
-$text = "En un lugar de la Mancha...";
-$user = "Jesus";
-$userName = "Quijote";
+// $text = "En un lugar de la Mancha...";
+// $user = "Jesus";
+// $userName = "Quijote";
+
+$arrays = [
+    [
+        "text" => "En un lugar de mi casa...",
+        "user" => "Cristina",
+        "userName" => "Panza"
+    ],
+    [
+        "text" => "Cuyo nombre no se",
+        "user" => "Manuela",
+        "userName" => "Ellie"
+    ],
+    [
+        "text" => "Cuyo nombre no quiero recordar",
+        "user" => "Manuel",
+        "userName" => "Sancho"
+    ]
+];
 
 require_once "index.html";
